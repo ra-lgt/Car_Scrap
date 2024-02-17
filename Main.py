267c8c9d50b9b0f3a5a881c6ed977954e9a1e69f
Ajay K <74807473+ra-lgt@users.noreply.github.com>
2024-02-17 05:54:16 +0530

    new class

--- Main.py
+++ Main.py
@@ -1,19 +1,19 @@
 from Kiji import Kiji
-
+from Kiji_Auto import Kiji_Auto
 
 
 
 class Main:
-    def start_scrap(self,url,type,choice):
+    def start_scrap(self,url,type,choice=None):
         if(type=="Kiji"):
             Kiji(url,choice)
         
         elif(type=="Kiji_auto"):
-            pass
+            Kiji_Auto(url)
         else:
             pass
 
 main=Main()
-main.start_scrap("https://www.kijiji.ca/","Kiji","cars")
+main.start_scrap("https://www.kijijiautos.ca/","Kiji_auto")
 
 
