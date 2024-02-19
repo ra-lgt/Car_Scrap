1d22b8ab04c90b4f2a1cb515b6dd6175b18ada66
ra-lgt <raviajay9344@gmail.com>
2024-02-19 20:16:19 +0530

    GSHEETS

--- Data_push.py
+++ Data_push.py
@@ -1,13 +1,21 @@
 import pandas as pd
 from datetime import datetime
+import Google_sheet
+import threading
 
 def push_data(data,name):
     data_to_append=pd.DataFrame(data)
 
     if(name=="Kijiji_Autos"):
         create_kiji_auto_id(data)
+        thread = threading.Thread(target=Google_sheet.push_data, args=(data, "Kijiji_Auto_Data"))
+        thread.start()
+        # Google_sheet.push_data(data=data,name="Kijiji_Auto_Data")
     elif(name=="Kijiji"):
         create_kiji_ids(data)
+        thread = threading.Thread(target=Google_sheet.push_data, args=(data, "Kijiji_Data"))
+        thread.start()
+        # Google_sheet.push_data(data=data,name="Kijiji_Data")
 
 
     excel_file_path = '{}.xlsx'.format(name)
@@ -19,8 +27,11 @@ def push_data(data,name):
         
     except FileNotFoundError:
         combined_data = data_to_append
-
-    combined_data.to_excel(excel_file_path, index=False)
+        
+    try:
+        combined_data.to_excel(excel_file_path, index=False)
+    except:
+        pass
 
 def create_kiji_auto_id(data):
     ids=data['ad_id']
@@ -35,8 +46,10 @@ def create_kiji_auto_id(data):
         
     except FileNotFoundError:
         combined_data = data_to_append
-
-    combined_data.to_excel(excel_file_path, index=False)
+    try:
+        combined_data.to_excel(excel_file_path, index=False)
+    except:
+        pass
 
 def create_kiji_ids(self,data):
     ids=data['ad_id']
@@ -51,8 +64,11 @@ def create_kiji_ids(self,data):
         
     except FileNotFoundError:
         combined_data = data_to_append
-
-    combined_data.to_excel(excel_file_path, index=False)
+        
+    try:
+        combined_data.to_excel(excel_file_path, index=False)
+    except:
+        pass
 
 def check_id(id,type):
     xl=None
---
+++ Google_sheet.py
@@ -0,0 +1,32 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import pandas as pd
+
+def authorize_google_sheets():
+    scope = ['https://spreadsheets.google.com/feeds',
+             'https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('Kijiji_Sheet_secret_token.json', scope)
+    client = gspread.authorize(creds)
+    return client
+
+def get_sheet(client, name):
+    sheet = client.open(name).sheet1
+    return sheet
+
+def append_data_to_sheet(sheet, data_to_append):
+    # If the sheet is empty, append the column names first
+    if not sheet.get_all_records():
+        sheet.append_row(data_to_append.columns.tolist())
+    
+    # Append the data
+    for row in data_to_append.values.tolist():
+        sheet.append_row(row)
+
+def push_data(data, name):
+    data_to_append = pd.DataFrame(data)
+
+    client = authorize_google_sheets()
+    sheet = get_sheet(client, name)
+    append_data_to_sheet(sheet, data_to_append)
+    
+
